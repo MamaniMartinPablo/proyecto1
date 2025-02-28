d7c45992994054abbded290df99f067182219e4b
mamanimartinpablo <mamanimartinpablo@gmail.com>
2025-02-28 16:46:41 -0300

    Primer ejemplo de un test

--- spec/prueba.spec.js
+++ spec/prueba.spec.js
@@ -3,3 +3,10 @@ describe("demo", function () {
     expect(4 + 2).toBe(6);
   });
 });
+
+
+describe("Primer ejemplo de test",()=>{
+  it('El numero 2 es igual a uno mas uno', () => {
+    expect(2).toEqual(1+1)
+  })
+})
